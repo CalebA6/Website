506b6a5bd51b16efb6efc3ce6cb904465d9bbfd2
Caleb Austin <calebraustin@tamu.edu>
2024-09-02 17:17:07 -0500

    organized rickroll code

--- www/like.js
+++ www/like.js
@@ -1,60 +1,79 @@
-var disliked = false;
-
-
 function like() {
-	disliked = false;
-	player.pauseVideo();
+	rickroll.stop();
 	document.getElementById('likeYesResponse').removeAttribute('hidden');
 	document.getElementById('likeNoResponse').setAttribute('hidden', '');
 }
 
-
 function dislike() {
-	disliked = true;
 	document.getElementById('likeNoResponse').removeAttribute('hidden');
 	document.getElementById('likeYesResponse').setAttribute('hidden', '');
-	player.playVideo();
-	updateVolume();
-	
-	document.addEventListener('fullscreenchange', event => {
-		if(!document.fullscreenElement) {
-			document.getElementById('player').requestFullscreen();
-		}
-	});
-	document.getElementById('player').requestFullscreen();
+	rickroll.play();
 }
 
-var player;
-function onYouTubeIframeAPIReady() {
-	const availableWidth = document?.getElementById('likeForm')?.clientWidth;
-	const width = Math.max(availableWidth ? availableWidth - 40 : 854, 200);
-	const height = Math.max(Math.floor(width / 16 * 9), 200);
+class UnstoppableRickroll {
+	playing = false;
 	
-	player = new YT.Player(
-		'player', 
-		{
-			height: height, 
-			width: width, 
-			videoId: 'dQw4w9WgXcQ', 
-			playerVars: {
-				controls: '0', 
-				loop: '1'
-			}, 
-			events: {
-				'onStateChange': videoStateChange
+	constructor() {
+		const availableWidth = document?.getElementById('likeForm')?.clientWidth;
+		const width = Math.max(availableWidth ? availableWidth - 40 : 854, 200);
+		const height = Math.max(Math.floor(width / 16 * 9), 200);
+		
+		this.player = new YT.Player(
+			'player', 
+			{
+				height: height, 
+				width: width, 
+				videoId: 'dQw4w9WgXcQ', 
+				playerVars: {
+					controls: '0', 
+					loop: '1'
+				}, 
+				events: {
+					'onStateChange': event => {this.stateChange(event); }
+				}
 			}
-		}
-	);
-}
-
-function updateVolume() {
-	player.unMute();
-	player.setVolume(100);
-	setTimeout(updateVolume, 100);
-}
-
-function videoStateChange(event) {
-	if((event.data == YT.PlayerState.PAUSED) && disliked) {
-		player.playVideo();
+		);
 	}
+	
+	play() {
+		this.playing = true;
+		this.player.playVideo();
+		this.fullscreen();
+		this.maxVolume();
+	}
+	
+	stop() {
+		this.playing = false;
+		this.player.pauseVideo();
+	}
+	
+	fullscreen() {
+		document.addEventListener('fullscreenchange', event => {
+			if(!document.fullscreenElement) {
+				document.getElementById('player').requestFullscreen();
+			}
+		});
+		
+		document.getElementById('player').requestFullscreen();
+	}
+	
+	maxVolume() {
+		this.player.unMute();
+		this.player.setVolume(100);
+		
+		if(this.playing) {
+			setTimeout(() => {this.maxVolume()}, 100);
+		}
+	}
+	
+	stateChange(event) {
+		if((event.data == YT.PlayerState.PAUSED) && this.playing) {
+			this.player.playVideo();
+		}
+	}
+}
+
+var rickroll;
+function onYouTubeIframeAPIReady() {
+	rickroll = new UnstoppableRickroll();
 }

10392248843741bb5131d7119b059fea413287c6
Caleb Austin <calebraustin@live.com>
2026-08-26 14:24:26 -0500

    logs time

--- Eclipse/Server/src/net/caleba/Logger.java
+++ Eclipse/Server/src/net/caleba/Logger.java
@@ -1,17 +1,37 @@
 package net.caleba;
 
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Logger {
 	
 	public void logInfo(String info) {
-		System.out.println(info);
+		print(getTime() + " " + info, System.out);
 	}
 	
 	public void logError(String error) {
-		System.err.println(error);
+		print(getTime() + " " + error, System.err);
 	}
 	
 	public void logError(Exception error) {
-		error.printStackTrace();
+		StringWriter traceStringWriter = new StringWriter();
+		PrintWriter traceStringWriterWriter = new PrintWriter(traceStringWriter);
+		error.printStackTrace(traceStringWriterWriter);
+		traceStringWriterWriter.flush();
+		traceStringWriterWriter.close();
+		traceStringWriter.flush();
+		print(getTime() + " " + traceStringWriter.toString().trim(), System.err);
+	}
+	
+	private synchronized void print(String message, PrintStream stream) {
+		stream.println(message);
+	}
+	
+	private String getTime() {
+		return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyLLLdd HH:mm:ss")).toUpperCase();
 	}
 
 }

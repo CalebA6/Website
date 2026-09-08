1e9fbbf5cb5bbbf17797b8009f01451811e65b65
Caleb Austin <calebraustin@live.com>
2026-09-08 13:33:22 -0500

    fixed SSL implementation

--- Eclipse/Server/src/net/caleba/Default.java
+++ Eclipse/Server/src/net/caleba/Default.java
@@ -1,14 +1,21 @@
 package net.caleba;
 import java.io.File;
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 
 public class Default {
 	
 	private static int port = 80;
 	private static String serverAddress = "http://localhost/";
 	private static File mainDirectory = new File("C:\\Users\\Caleb\\OneDrive\\Documents\\Projects\\WebsiteSSL\\www");
+	private static File keyStoreLocation = new File("C:\\Users\\Caleb\\OneDrive\\Documents\\Projects\\WebsiteSSL\\keystore");
+	private static String keyStorePassword = null;
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, UnrecoverableKeyException, KeyManagementException, KeyStoreException, NoSuchAlgorithmException, CertificateException {
 		//Handles Program Settings
 		if(args.length > 0) {
 			port = Integer.parseInt(args[0]);
@@ -18,6 +25,12 @@ public class Default {
 		}
 		if(args.length > 2) {
 			mainDirectory = new File(args[2]);
+		}
+		if(args.length > 3) {
+			keyStoreLocation = new File(args[3]);
+		}
+		if(args.length > 4) {
+			keyStorePassword = args[4];
 		}
 		//Starts Server
 		new Server();
@@ -33,6 +46,14 @@ public class Default {
 	
 	public static File getMainDirectory() {
 		return mainDirectory;
+	}
+	
+	public static File getKeyStoreLocation() {
+		return keyStoreLocation;
+	}
+	
+	public static String getKeyStorePassword() {
+		return keyStorePassword;
 	}
 	
 }
--- Eclipse/Server/src/net/caleba/Server.java
+++ Eclipse/Server/src/net/caleba/Server.java
@@ -1,13 +1,23 @@
 package net.caleba;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.util.Arrays;
 
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLServerSocket;
-import javax.net.ssl.SSLServerSocketFactory;
 
 public class Server{
 
-	public Server() throws IOException {
+	public Server() throws IOException, UnrecoverableKeyException, KeyManagementException, KeyStoreException, NoSuchAlgorithmException, CertificateException {
 		new SecureServer().start();
 		ServerSocket server = new ServerSocket(Default.getPort());
 		while(true) {
@@ -19,11 +29,24 @@ public class Server{
 
 class SecureServer extends Thread {
 	
+	private SSLServerSocket server;
+	
+	public SecureServer() throws KeyStoreException, NoSuchAlgorithmException, CertificateException, FileNotFoundException, IOException, UnrecoverableKeyException, KeyManagementException {
+		KeyStore keyStore = KeyStore.getInstance("PKCS12");
+		KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+		char[] password = getKeyStorePassword();
+		keyStore.load(new FileInputStream(Default.getKeyStoreLocation()), password);
+		keyManagerFactory.init(keyStore, password);
+		Arrays.fill(password, '\0');
+		SSLContext sslContext = SSLContext.getInstance("TLS");
+		sslContext.init(keyManagerFactory.getKeyManagers(), null, null);
+		server = (SSLServerSocket) sslContext.getServerSocketFactory().createServerSocket(443);
+		server.setEnabledProtocols(new String[] {"TLSv1.3"});
+		server.setEnabledCipherSuites(new String[] {"TLS_AES_128_GCM_SHA256"});
+	}
+	
 	public void run() {
 		try {
-			SSLServerSocket server = (SSLServerSocket) SSLServerSocketFactory.getDefault().createServerSocket(443);
-			server.setEnabledProtocols(new String[] {"TLSv1.3"});
-			server.setEnabledCipherSuites(new String[] {"TLS_AES_128_GCM_SHA256"});
 			while(true) {
 				new Connection(server.accept(), true).start();
 			}
@@ -31,6 +54,13 @@ class SecureServer extends Thread {
 			Logger.logGeneralError("Secure Server Failed: " + e.toString());
 			System.exit(1);
 		}
+	}
+	
+	private char[] getKeyStorePassword() {
+		String password = Default.getKeyStorePassword();
+		if(password != null) return password.toCharArray();
+		System.out.print("Enter key store password: ");
+		return System.console().readPassword();
 	}
 	
 }
